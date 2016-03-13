c1ca386465d96bb9c00e93cd647465e1f683cb77
墨染 <184918211@qq.com>
2016-03-13 22:20:45 +0800

    3-13 ajax

--- 3.http.js
+++ 3.http.js
@@ -1,12 +1,29 @@
-//导入http模块
-var http=require("http");
-
-var server=http.createServer(function (req,resp) {
-    var date=new Date();
-    resp.setHeader()
-    resp.write(date.getFullYear().toString()+'年'+date.getMonth().toString()+"月"+date.getDate().toString()+"日");
-    resp.end();
-});
-
-server.listen(8080,'127.0.0.1');
-
+////导入http模块
+//var http=require("http");
+//var fs=require("fs");
+//var server=http.createServer();
+//server.on('request',function (req,resp) {
+//    if(req.url === '/data.txt'){
+//        console.log("开始查ajax");
+//        fs.readFile('./paixu/data.txt', function(err, data){
+//            if(err){
+//                console.log(err);
+//            }else {
+//                console.log(data);
+//                resp.end(data)
+//            }
+//
+//        });
+//    }else
+//    if(req.url === '/'){
+//        fs.readFile("./paixu/index.html",'utf-8', function (err,data) {
+//            if(err){
+//                console.log(err)
+//            }else {
+//                resp.end(data)
+//            }
+//        });
+//    }
+//});
+//server.listen(8090,'127.0.0.1');
+//----------------------------------------------------------------
