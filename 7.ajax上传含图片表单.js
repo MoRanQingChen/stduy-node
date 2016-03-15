b1ea507bf012c312f844536a5bb1568b0580a994
墨染 <184918211@qq.com>
2016-03-15 17:55:10 +0800

    3-15
    formidable中间件使用

--- 7.ajax上传含图片表单.js
+++ 7.ajax上传含图片表单.js
@@ -1,6 +1,8 @@
 var http=require('http');
 var fs=require('fs');
 var url=require('url');
+var formidable=require('formidable')//用于解析含文件表单,本节主角
+var util=require('util');
 http.createServer(function(req,res){
     var urlObj=url.parse(req.url);
     if(urlObj.path=='/'){
@@ -8,13 +10,20 @@ http.createServer(function(req,res){
             res.end(data);
         })
     }else if(urlObj.path='/reg') {
-        var str='';
-        req.on('data',function(data){
-            str+=data;
-        })
-        req.on('end',function(){
-            console.log(str)
-        })
+        //-------------------------------------------------------------
+      //先阅读formidable中间件的readMe,以下内容是从example中拷贝内容
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            //fs.writeFile('html/regPic/1.jpg',files[0])
+            //console.log(files)
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('received upload:\n\n');
+            //下面用到了util中间件,需引入.其中util.inspect是将对象转换为字符串
+            res.end(util.inspect({fields: fields, files: files}));
+            console.log(files)
+        });
+        return;
+        //------------------------------------------------------------
     }else{
 
     }
