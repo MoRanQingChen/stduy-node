12692a864f27cf2595e79dc844d167712fc6a12c
墨染 <184918211@qq.com>
2016-03-16 21:14:10 +0800

    3-16

--- 1.全局对象.js
+++ 1.全局对象.js
@@ -34,3 +34,27 @@ process.nextTick(function () {
 console.log("我是普通的")
 //所以优先级排序  普通>process.nextTick>setImmediate
 //-----------------------------------------
+//下面是util方法
+//----------------------------------
+//用util完成一个构造函数的[私有]继承(所谓私有继承,就是可以用父类prototype内的函数)
+var util=require("util");
+function Parent(){
+    this.name="parent";
+    this.age=50;
+    this.say= function () {
+        console.log("my name is "+this.name)
+    }
+}
+Parent.prototype.sayHello= function () {
+    console.log("hello "+this.name)//注意this的关系
+}
+function Child(){
+    Parent.call(this);//普通继承
+    this.name="child";//注意先parent的call再写自己的属性去覆盖父类的
+}
+//用util.inherit处理一下 , 子类也有父类的prototype内的方法了
+util.inherits(Child,Parent);//继承私有内容(只继承prototype内的属性)
+var c=new Child();
+console.log(c);//c.sayHello是不存在的
+//因为util.inherit处理过,所以可以用父类的prototype内的,
+c.sayHello();
